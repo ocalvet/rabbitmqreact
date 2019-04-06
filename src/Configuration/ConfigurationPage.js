c661a6f1fad185d083ac792afcec82c0b368d0e9
Ovidio R. Calvet <calveto@gmail.com>
2019-04-06 14:16:13 -0400

    Change typography variant

--- src/Configuration/ConfigurationPage.js
+++ src/Configuration/ConfigurationPage.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
 const ConfigurationPage = () => {
-    return (
-        <div>
-            <Typography variant="title">Configuration</Typography>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Typography variant="h5">Configuration</Typography>
+    </div>
+  );
+};
 
 export default ConfigurationPage;
